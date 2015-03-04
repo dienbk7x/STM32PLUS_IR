d5bc624eb85e4ce308e09a0b03486454cf0d9a72
joaoc <joao.c.cabral@gmail.com>
2015-03-04 16:20:05 +0000

    Moved to multiple files. Added the RC5 decode capability (to be tested)

---
+++ IR_RC5.h
@@ -0,0 +1,14 @@
+#ifndef IR_RC5_H_INCLUDED
+#define IR_RC5_H_INCLUDED
+
+#define RC5_N_BITS 14
+
+#define RC5_MIN  69//(89-10)
+#define RC5_MAX  99//(89+10)
+
+#define RC5_2_MIN  138//(2*(89-10))
+#define RC5_2_MAX  198//(2*(89+10))
+
+
+
+#endif /* IR_RC5_H_INCLUDED */
